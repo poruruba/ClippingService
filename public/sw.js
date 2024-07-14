a3862ff1af7a36af48f52603dff4429f18e1418a
poruruba <44470781+poruruba@users.noreply.github.com>
2024-07-15 00:37:30 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('sw event: fetch called');
+//  console.log('sw event: fetch called');
 
   event.respondWith(
     caches.match(event.request)
